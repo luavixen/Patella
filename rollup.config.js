929d47edda94e607fe8c9d3b485901a1f9ae97b8
Lua MacDougall <lua@foxgirl.dev>
2021-02-11 12:30:19 -0800

    Removed unneeded dist files and fixed issue when importing with require()
    I have reduced the number of dist outputs to only two, as the other files had
    no real usecases. There is now only patella.cjs.js for old CommonJS
    environments and patella.iife.min.js for browsers.
    Also, Node.js was throwing errors when running `require("patella")`, which has
    now been fixed by adding a package.json file into the dist folder with the
    content: `{ "type": "commonjs" }`

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,31 @@
 import { minify as minifyTerser } from "terser";
 import { minify as minifyUglify } from "uglify-js";
 
+import { mkdir, writeFile } from "fs/promises";
+
 const uglify = {
   name: "uglify",
   config: {
-    ecma: 5,
     ie8: true,
     webkit: true,
+    v8: true,
     compress: {
       passes: 4,
       hoist_funs: true,
       hoist_vars: true,
+      hoist_props: true,
+      inline: 2,
+      reduce_funcs: false,
       typeofs: false,
       unsafe_comps: true
+    },
+    output: {
+      comments: true
     }
   },
   renderChunk(code, chunk, options) {
     const result = minifyUglify(code, {
-      ...this.config,
+      ...uglify.config,
       sourceMap: !!options.sourcemap,
       toplevel: /^c(ommon)?js$/.test(options.format)
     });
@@ -36,6 +44,7 @@ const terser = {
       collapse_vars: true,
       hoist_funs: true,
       hoist_vars: true,
+      hoist_props: true,
       typeofs: false,
       unsafe_comps: true,
       pure_funcs: [
@@ -45,7 +54,7 @@ const terser = {
   },
   renderChunk(code, chunk, options) {
     return minifyTerser(code, {
-      ...this.config,
+      ...terser.config,
       sourceMap: !!options.sourcemap,
       module: /^(esm?|module)$/.test(options.format),
       toplevel: /^c(ommon)?js$/.test(options.format)
@@ -53,8 +62,17 @@ const terser = {
   }
 };
 
-const input = (input, ...outputs) => ({ input, output: outputs });
-const output = (file, format, sourcemap = true, ...plugins) => ({
+const packageCJS = {
+  name: "package-cjs",
+  async buildEnd(error) {
+    if (error) return;
+    try { await mkdir("./dist"); } catch (error) {}
+    await writeFile("./dist/package.json", `{ "type": "commonjs" }\n`);
+  }
+};
+
+const input = (input, plugins = [], ...output) => ({ input, output, plugins });
+const output = (file, format, sourcemap = true, plugins = []) => ({
   file,
   format,
   sourcemap,
@@ -66,11 +84,7 @@ const output = (file, format, sourcemap = true, ...plugins) => ({
   freeze: false
 });
 
-export default input("lib/patella.js",
-  output("dist/patella.esm.js", "esm"),
+export default input("lib/patella.js", [packageCJS],
   output("dist/patella.cjs.js", "cjs"),
-  output("dist/patella.iife.js", "iife"),
-  output("dist/patella.esm.min.js", "esm", "hidden", terser),
-  output("dist/patella.cjs.min.js", "cjs", "hidden", uglify, terser),
-  output("dist/patella.iife.min.js", "iife", "hidden", uglify, terser)
+  output("dist/patella.iife.min.js", "iife", "hidden", [uglify, terser])
 );
